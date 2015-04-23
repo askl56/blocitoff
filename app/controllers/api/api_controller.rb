907555bf24dd9381569e877a5d0b919a62b74b27
askl56 <amscott@openmailbox.org>
2015-04-23 02:14:34 +0100

    finished adding API functionality

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,6 +1,25 @@
 module Api
   class ApiController < ApplicationController
     skip_before_filter :verify_authenticity_token
+    protect_from_forgery with: :null_session
+    before_filter :authenticate
+
+    def current_user
+      @current_user
+    end
+
+
+    def authenticate
+      Rails.logger.info "API authentication:#{email} #{password}"
+      user = User.find_by(params[:id])
+      if user && user.authenticate(password)
+        Rails.logger.info "Logging in #{user.inspect}"
+        true
+      else
+        Rails.logger.warn "No valid credentials"
+        false
+      end
+    end
 
   end
 end
--- spec/controllers/todo_items_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-describe TodoItemsController do
-  
-end
