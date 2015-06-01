9bc27bfa9b8eb68e08975f0f1b0f2a7489f61933
askl56 <amscott@openmailbox.org>
2015-06-01 02:33:50 +0100

    fixed to match ruby 1.9 hash syntax

--- spec/controllers/regression/todo_items_controller_spec.rb
+++ spec/controllers/regression/todo_items_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe TodoItemsController, regressor: true do
   # === Routes (REST) ===
-  it { should route(:patch, '/todo_lists/1/todo_items/1/complete').to('todo_items#complete', {:todo_list_id=>"1", :id=>"1"}) } 
-	it { should route(:post, '/todo_lists/1/todo_items').to('todo_items#create', {:todo_list_id=>"1"}) } 
-	it { should route(:delete, '/todo_lists/1/todo_items/1').to('todo_items#destroy', {:todo_list_id=>"1", :id=>"1"}) } 
+  it { should route(:patch, '/todo_lists/1/todo_items/1/complete').to('todo_items#complete', {todo_list_id:"1", id:"1"}) } 
+	it { should route(:post, '/todo_lists/1/todo_items').to('todo_items#create', {todo_list_id:"1"}) } 
+	it { should route(:delete, '/todo_lists/1/todo_items/1').to('todo_items#destroy', {todo_list_id:"1", id:"1"}) } 
   # === Callbacks (Before) ===
   it { should use_before_filter(:verify_authenticity_token) }
 	it { should use_before_filter(:set_xhr_redirected_to) }
--- spec/controllers/regression/todo_lists_controller_spec.rb
+++ spec/controllers/regression/todo_lists_controller_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe TodoListsController, regressor: true do
   # === Routes (REST) ===
   it { should route(:post, '/todo_lists').to('todo_lists#create', {}) } 
-	it { should route(:delete, '/todo_lists/1').to('todo_lists#destroy', {:id=>"1"}) } 
-	it { should route(:get, '/todo_lists/1/edit').to('todo_lists#edit', {:id=>"1"}) }
+	it { should route(:delete, '/todo_lists/1').to('todo_lists#destroy', {id:"1"}) } 
+	it { should route(:get, '/todo_lists/1/edit').to('todo_lists#edit', {id:"1"}) }
 	it { should route(:get, '/todo_lists').to('todo_lists#index', {}) }
 	it { should route(:get, '/todo_lists/new').to('todo_lists#new', {}) }
-	it { should route(:get, '/todo_lists/1').to('todo_lists#show', {:id=>"1"}) }
-	it { should route(:patch, '/todo_lists/1').to('todo_lists#update', {:id=>"1"}) } 
+	it { should route(:get, '/todo_lists/1').to('todo_lists#show', {id:"1"}) }
+	it { should route(:patch, '/todo_lists/1').to('todo_lists#update', {id:"1"}) } 
   # === Callbacks (Before) ===
   it { should use_before_filter(:verify_authenticity_token) }
 	it { should use_before_filter(:set_xhr_redirected_to) }
