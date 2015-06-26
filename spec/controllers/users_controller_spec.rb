3222359efe9aa0aa32e0b82f1b926dc7b06a06c6
askl56 <amscott@openmailbox.org>
2015-06-26 23:42:46 +0100

    added helpers

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 describe UsersController do
+#   describe "GET New" do
+#     context 'new user' do
+#       it "has a 200 status code" do
+#         sign_in
+#         get :index
+#         expect(response.status).to eq(200)
+#       end
+#     end
+#   end
 end
