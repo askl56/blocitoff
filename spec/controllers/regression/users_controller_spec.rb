001844edc9c8eba548fc39275a13ecbd9f443cd3
askl56 <amscott@openmailbox.org>
2015-10-23 21:52:48 +0100

    tests cleaned up

--- spec/controllers/regression/users_controller_spec.rb
+++ spec/controllers/regression/users_controller_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe UsersController, regressor: true do
   # === Routes (REST) ===
-  it { should route(:post, '/users').to('users#create', {}) }
-  it { should route(:get, '/signup').to('users#new', {}) }
+  it { is_expected.to route(:post, '/users').to('users#create', {}) }
+  it { is_expected.to route(:get, '/signup').to('users#new', {}) }
   # === Callbacks (Before) ===
-  it { should use_before_filter(:verify_authenticity_token) }
-  it { should use_before_filter(:set_xhr_redirected_to) }
-  it { should use_before_filter(:set_request_method_cookie) }
-  it { should use_before_filter(:authorize) }
+  it { is_expected.to use_before_filter(:verify_authenticity_token) }
+  it { is_expected.to use_before_filter(:set_xhr_redirected_to) }
+  it { is_expected.to use_before_filter(:set_request_method_cookie) }
+  it { is_expected.to use_before_filter(:authorize) }
   # === Callbacks (After) ===
-  it { should use_after_filter(:abort_xdomain_redirect) }
-  it { should use_after_filter(:verify_same_origin_request) }
+  it { is_expected.to use_after_filter(:abort_xdomain_redirect) }
+  it { is_expected.to use_after_filter(:verify_same_origin_request) }
   # === Callbacks (Around) ===
 end
